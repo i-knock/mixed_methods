2f80b42c1b596c41d2f58b4c14af4b4c9cb47ae7
i-knock <a.sanchezmartin@student.utwente.nl>
2022-03-22 10:54:07 +0100

    Added double LDA approach

--- twitter_import.py
+++ twitter_import.py
@@ -2,44 +2,44 @@
 # https://github.com/twintproject/twint/wiki/Configuration
 import twint
 
-values_to_search = [
-    "#ABPSouthampton",
-    "#DP_World",
-    "#GaPorts",
-    "#PortofHamburg",
-    "#HutchisonPPC",
-    "#LondonPortAuth",
-    "#PANYNJ",
-    "#portdebarcelona",
-    "#PortMTL",
-    "#Port_Houston",
-    "#PortofAntwerp",
-    "#felixstowe_port",
-    "#portoflongbeach",
-    "#PortofLA",
-    "#PortofOakland",
-    "#PortOfRotterdam",
-    "#PortofSeattle",
-    "#PortVancouver",
-    "#Port_Zeebrugge",
-    "#portodigenova",
-    "#PuertoAlgeciras",
-    "#PuertodeCtg",
-    "#SCPorts",
-    "#PortofVirginia",
-    "#AutPortValencia"
-]
+# values_to_search = [
+#     "#ABPSouthampton",
+#     "#DP_World",
+#     "#GaPorts",
+#     "#PortofHamburg",
+#     "#HutchisonPPC",
+#     "#LondonPortAuth",
+#     "#PANYNJ",
+#     "#portdebarcelona",
+#     "#PortMTL",
+#     "#Port_Houston",
+#     "#PortofAntwerp",
+#     "#felixstowe_port",
+#     "#portoflongbeach",
+#     "#PortofLA",
+#     "#PortofOakland",
+#     "#PortOfRotterdam",
+#     "#PortofSeattle",
+#     "#PortVancouver",
+#     "#Port_Zeebrugge",
+#     "#portodigenova",
+#     "#PuertoAlgeciras",
+#     "#PuertodeCtg",
+#     "#SCPorts",
+#     "#PortofVirginia",
+#     "#AutPortValencia"
+# ]
 
-values_to_search = ['"port" AND "expansion"'] # both words should be included
+# values_to_search = ['"port" AND "expansion"'] # both words should be included
 
 values_to_search = ['port'] # only port
 
-values_to_search = ['"harbour" OR "harbor"']
+# values_to_search = ['"harbour" OR "harbor"']
 
 c = twint.Config()
 for value in values_to_search:
     # c.Limit = 1000
-    file_name = f"./twitter_data_{value}.csv"
+    file_name = f"./mega_twitter_data_{value}.csv"
     c.Output = file_name
     c.Store_csv = True
     c.Search = value
